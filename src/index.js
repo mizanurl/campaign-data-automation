db4387e7bcc96349eebafdd00756e3c8ee83bf65
mizanurl <cicakemizan@gmail.com>
2024-10-17 00:12:10 +0600

    Removed Debugging

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ app.post('/webhook', (req, res) => {
         const serverWebhook = process.env.WEBHOOK_URL;
 
         // Step 1: Fetch user data
-        axios.get(`https://graph.facebook.com/v20.0/${userId}?debug=all`, {
+        axios.get(`https://graph.facebook.com/v20.0/${userId}`, {
             headers: {
                 Authorization: `Bearer ${accessToken}`
             },
